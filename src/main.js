a198995e5334f1ab421cf88bcc410a770c646897
kaluginavioletta <kalugina7bk.ru@yandex.ru>
2024-02-11 21:43:49 +0700

    начальный вариант кода для привязки атрибутов

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ let app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks'
+        description: 'A pair of warm, fuzzy socks',
+        image: "src/assets/vmSocks-green-onWhite.jpg"
     }
  })
  
