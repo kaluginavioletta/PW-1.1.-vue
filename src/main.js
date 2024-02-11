7a4d337fdb9f1dec5c85ed8decee91abeb3363bc
kaluginavioletta <kalugina7bk.ru@yandex.ru>
2024-02-11 23:57:18 +0700

    добавление атрибута и метода для добавления товара и добавление атрибутов в массиве variants

--- src/main.js
+++ src/main.js
@@ -7,17 +7,24 @@ let app = new Vue({
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green'
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue'
-            }
-         ],
-         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']         
-    }
+           {
+               variantId: 2234,
+               variantColor: 'green',
+               variantImage: "src/assets/vmSocks-green-onWhite.jpg"
+           },
+           {
+               variantId: 2235,
+               variantColor: 'blue',
+               variantImage: "src/assets/vmSocks-blue-onWhite.jpg"
+           }
+       ],
+       cart: 0,    
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
+     } 
  })
  
  
