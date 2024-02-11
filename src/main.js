819e3c4f0a3d878c1682e29a2fe76de1a0b28bed
kaluginavioletta <kalugina7bk.ru@yandex.ru>
2024-02-11 21:55:09 +0700

    вывод изображения синих носков, а также вывод altText

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ let app = new Vue({
     data: {
         product: 'Socks',
         description: 'A pair of warm, fuzzy socks',
-        image: "src/assets/vmSocks-green-onWhite.jpg"
+        image: "src/assets/vmSocks-blue-onWhite.jpg",
+        altText: "A pair of socks"
     }
  })
  
