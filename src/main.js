d833bf7fb06e1d33e1f351a437945dd0a6c8d990
kaluginavioletta <kalugina7bk.ru@yandex.ru>
2024-02-11 23:28:58 +0700

    вывод списка на странице и массива вариантов товара

--- src/main.js
+++ src/main.js
@@ -5,7 +5,17 @@ let app = new Vue({
         image: "src/assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        onSale: true
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+         ]         
     }
  })
  
